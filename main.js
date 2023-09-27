b905a7bf9aeaeaba734bd1aec9857835b0a50a48
Camiilo Piinto <camiilo.piinto@gmail.com>
2023-09-27 13:00:43 -0500

    reajuste al tamaño de las imagenes y archivo .js

---
+++ main.js
@@ -0,0 +1,10 @@
+
+ScrollReveal().reveal('.sobre-mi');
+ScrollReveal().reveal('.name');
+ScrollReveal().reveal('.PI, .IPI', {delay: 500});
+ScrollReveal().reveal('.main-body', {delay: 500});
+ScrollReveal().reveal('.Knowledges', {delay: 700});
+ScrollReveal().reveal('.habilidades-nav', {delay: 500});
+ScrollReveal().reveal('.habilidades-list', {delay: 700});
+ScrollReveal().reveal('.nav-referencias', {delay: 500});
+ScrollReveal().reveal('.referenciado-grupo', {delay: 700});
