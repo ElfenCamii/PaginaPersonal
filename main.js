25586f6eae3a758c7bcbb61da71391a4529605b7
Camiilo Piinto <camiilo.piinto@gmail.com>
2023-09-28 13:27:00 -0500

    Se agrego un boton para el modo claro y oscuro Ademas de esto, se creo el nuevo diseño claro

--- main.js
+++ main.js
@@ -8,3 +8,11 @@ ScrollReveal().reveal('.habilidades-nav', {delay: 500});
 ScrollReveal().reveal('.habilidades-list', {delay: 700});
 ScrollReveal().reveal('.nav-referencias', {delay: 500});
 ScrollReveal().reveal('.referenciado-grupo', {delay: 700});
+
+let toggle = document.getElementById('modo');
+let body = document.querySelector('body');
+
+toggle.onclick = function(){
+    toggle.classList.toggle('active');
+    body.classList.toggle('active');
+}
