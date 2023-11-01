3564b398ca0725e2d5505a8046c152bef67b8a4e
Camiilo Piinto <camiilo.piinto@gmail.com>
2023-11-01 16:10:11 -0500

    Add info de la pagina de Tesla

--- js/webs.js
+++ js/webs.js
@@ -1,8 +1,10 @@
 window.onload = function(){
     document.querySelector('.cont_modal').className = "cont_modal";
+    document.querySelector('.cont_modal0').className = "cont_modal0";  
     document.querySelector('.cont_modal1').className = "cont_modal1";  
 }
 var c = 0;
+var c0 = 0;
 var c1 = 0;
 function open_close(){
     if(c % 2 == 0){    
@@ -11,6 +13,15 @@ function open_close(){
     }else {
         document.querySelector('.cont_modal').className = "cont_modal"; 
      c++;    
+    }  
+}
+function open_close0(){
+    if(c0 % 2 == 0){    
+        document.querySelector('.cont_modal0').className = "cont_modal0 cont_modal_active0"; 
+        c0++;
+    }else {
+        document.querySelector('.cont_modal0').className = "cont_modal0"; 
+     c0++;    
     }  
 }
 function open_close1(){
